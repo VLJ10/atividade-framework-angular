1b2954f50a827fc75135282620f5a06a9bfceb2b
Vinicius <vinicius.oliveira100307@gmail.com>
2026-02-06 14:32:00 -0300

    codigo app.ts

--- page-dog/src/app/app.ts
+++ page-dog/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 interface DogApiResponse {
   message: string;
@@ -9,28 +11,42 @@ interface DogApiResponse {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('page-dog');
 
-  imagemDog = signal('') 
+  imagemDog = signal('')
+  mostrarGaleria = false
+  nomeDog: string = ''
 
   async buscarDog() {
+
+
+    let url = 'https://dog.ceo/api/breeds/image/random'
+
+    if (this.nomeDog.trim() !== '') {
+      url = `https://dog.ceo/api/breed/${this.nomeDog.toLowerCase()}/images/random`
+    }
+
+
     try {
+      const response = await fetch(url)
+      const data = await response.json()
 
-      const response = await fetch('https://dog.ceo/api/breeds/image/random');
-      const data: DogApiResponse = await response.json();
-
-      console.log('Resposta da API:', data);
-      this.imagemDog.set(data.message);
-
+      this.imagemDog.set(data.message)
     } catch (error) {
       console.error('Erro ao buscar dog:', error);
     }
   }
+
+  trocarTela() {
+    this.mostrarGaleria = true
+    console.log('mostraGaleria:', this.mostrarGaleria)
+  }
+
 
   constructor() {
     this.buscarDog();
